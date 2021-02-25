b58b8ce54c1217454d403d894f244fc6f129d1d6
NyafiRawr <leonid.b4@yandex.ru>
2021-02-26 05:59:29 +0700

    Хранение расписания работы

---
+++ src/models/holiday.model.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, Sequelize) => {
+    const Model = sequelize.define(
+        'holiday',
+        {
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false,
+            },
+            queueId: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            // Конкретный день, конкретного года
+            date: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            // 1 - праздник, 0 - рабочий день
+            isHoliday: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+            },
+        },
+        {
+            paranoid: false,
+            timestamps: true,
+        }
+    );
+
+    return Model;
+};
---
+++ src/models/schedule.model.js
@@ -0,0 +1,68 @@
+const dayNames = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+
+module.exports = (sequelize, Sequelize) => {
+    const Model = sequelize.define(
+        'schedule',
+        {
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false,
+            },
+            queueId: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            // Время начала рабочего дня
+            startTime: {
+                type: Sequelize.TIME,
+                allowNull: false,
+            },
+            // Время окончания рабочего дня
+            endTime: {
+                type: Sequelize.TIME,
+                allowNull: false,
+            },
+            // Дни недели: битовая маска бит1(2)=Пн, бит7(128)=Вс, Пн-Пт=62
+            weekday: {
+                type: Sequelize.INTEGER,
+                defaultValue: 62,
+                allowNull: false,
+                get() {
+                    const rest = [];
+                    let result = this.weekday;
+                    while (rest.length !== 7) {
+                        result = Math.floor(result / 2);
+                        rest.push(result % 2);
+                    }
+                    return rest
+                        .map((day, index) => {
+                            if (day === 1) {
+                                return dayNames[index];
+                            }
+                            return null;
+                        })
+                        .filter((value) => value !== null);
+                },
+            },
+            //  Дата начала действия записи расписания
+            workFrom: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            // Дата окончания действия
+            workTo: {
+                type: Sequelize.DATE,
+                defaultValue: '3000-01-01',
+                allowNull: false,
+            },
+        },
+        {
+            paranoid: false,
+            timestamps: true,
+        }
+    );
+
+    return Model;
+};
