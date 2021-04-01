05aba8425361c1e27600697b6e9ff59b8f9dee40
NyafiRawr <leonid.b4@yandex.ru>
2021-04-01 19:40:46 +0700

    Обещания заменаны на ожидания, добавлено включение веб-сокета

--- src/services/web-server.js
+++ src/services/web-server.js
@@ -1,103 +1,91 @@
 const http = require('http');
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const errorHandler = require('../handlers/error.handler');
 const rateLimit = require('express-rate-limit');
 const slowDown = require('express-slow-down');
+const helmet = require('helmet');
+
+const webSocket = require('./web-socket');
+const log = require('../logger');
+const errorHandler = require('../handlers/error.handler');
+
 const { homepage } = require('../../package.json');
 const { server } = require('../config.js');
-const helmet = require('helmet');
-const log = require('../logger');
 
 let httpServer;
 
-const initialize = () => {
-    return new Promise((resolve, reject) => {
-        const app = express();
+const initialize = async () => {
+    const app = express();
 
-        httpServer = http.createServer(app);
+    httpServer = http.createServer(app);
 
-        // Обработка content-type: application/json
-        app.use(express.json());
+    // Обработка content-type: application/json
+    app.use(express.json());
 
-        // Обработка куков
-        app.use(cookieParser());
+    // Обработка куков
+    app.use(cookieParser());
 
-        // Ограничение скорости за спам запросами
-        const speedLimiter = slowDown(server.limit.speed);
-        app.use(speedLimiter);
+    // Ограничение скорости за спам запросами
+    const speedLimiter = slowDown(server.limit.speed);
+    app.use(speedLimiter);
 
-        // Блокировка за спам запросами
-        const rateLimiter = rateLimit(server.limit.rate);
-        app.use(rateLimiter);
+    // Блокировка за спам запросами
+    const rateLimiter = rateLimit(server.limit.rate);
+    app.use(rateLimiter);
 
-        // Защита HTTP-заголовков (содержит в себе 11 проверок)
-        app.use(helmet());
+    // Защита HTTP-заголовков (содержит в себе 11 проверок)
+    app.use(helmet());
 
-        //#region Маршруты
+    await webSocket.initialize(httpServer);
 
-        app.get('/', (request, response) => {
-            response.redirect('/api');
-        });
+    //#region Маршруты
 
-        app.get('/api', (request, response) => {
-            response.redirect(homepage);
-        });
-
-        app.use('/api/v2/users', require('../controllers/users.controller'));
-        app.use('/api/v2/queues', require('../controllers/queues.controller'));
-        app.use(
-            '/api/v2/positions',
-            require('../controllers/positions.controller')
-        );
-        app.use(
-            '/api/v2/schedules',
-            require('../controllers/schedules.controller')
-        );
-        app.use(
-            '/api/v2/holidays',
-            require('../controllers/holidays.controller')
-        );
-
-        //#endregion
-
-        app.use(errorHandler);
-
-        // Если искомого адреса не существует, то выдать ошибку с кодом 404
-        app.use('*', (request, response) => {
-            response.status(404).send();
-        });
-
-        const webServer = httpServer
-            .listen(server.port, server.address)
-            .on('listening', () => {
-                log.info(
-                    `Веб-сервер доступен по адресу: http://${
-                        webServer.address().address
-                    }:${webServer.address().port}`
-                );
-                resolve();
-            })
-            .on('error', (err) => {
-                reject(err);
-            });
+    app.get('/', (request, response) => {
+        response.redirect('/api');
     });
+
+    app.get('/api', (request, response) => {
+        response.redirect(homepage);
+    });
+
+    app.use('/api/v2/users', require('../controllers/users.controller'));
+
+    app.use('/api/v2/queues', require('../controllers/queues.controller'));
+
+    app.use(
+        '/api/v2/positions',
+        require('../controllers/positions.controller')
+    );
+
+    app.use(
+        '/api/v2/schedules',
+        require('../controllers/schedules.controller')
+    );
+
+    app.use('/api/v2/holidays', require('../controllers/holidays.controller'));
+
+    //#endregion
+
+    app.use(errorHandler);
+
+    // Если искомого адреса не существует, то выдать ошибку с кодом 404
+    app.use('*', (request, response) => {
+        response.status(404).send();
+    });
+
+    const webServer = httpServer
+        .listen(server.port, server.address)
+        .on('listening', () => {
+            log.info(
+                `Веб-сервер доступен по адресу: http://${
+                    webServer.address().address
+                }:${webServer.address().port}`
+            );
+        });
 };
 
-// Функция close возвращает promise, который разрешается при успешном
-// закрытии веб-сервера. Метод httpServer.close останавливает установление
-// новых соединений, но не заставляет закрывать уже открытые соединения
-const close = () => {
-    return new Promise((resolve, reject) => {
-        httpServer.close((err) => {
-            if (err) {
-                reject(err);
-                return;
-            }
-
-            resolve();
-        });
-    });
+const close = async () => {
+    httpServer.close();
 };
 
 module.exports = {
